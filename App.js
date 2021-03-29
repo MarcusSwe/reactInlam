b11295d4715bc3a51b79abf2cd3f06ef936af8da
unknown <marcus.larsen.swe@gmail.com>
2021-03-30 01:54:32 +0200

    pokemin print egen komponent

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import PrintaPokemon from './printaPokemon';
+
+function App() {
+  
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    async function fetchPokemons(){
+      const resp = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+      const pokemon = await resp.json()
+      const pokemon50 = pokemon.results.slice(0,49)
+      setPokemons(pokemon50);      
+      console.log(pokemon50)
+    }
+    fetchPokemons();        
+  },[]);
+
+  
+
+ function removePokemon(x){
+   const removed = [...pokemons];
+   removed.splice(x,1);   
+   setPokemons(removed);
+  }
+
+
+
+  return (
+        <div>
+        <PrintaPokemon 
+        poke={pokemons}
+        removePokemon = {removePokemon}
+        />        
+      </div>  
+  )
+ 
+}
+
+
+
+export default App;
+
---
+++ printaPokemon.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function PrintaPokemon(props){
+
+     return(
+        <div>
+        {props.poke.map((p, index) => <div key={p.name}  index={index} className="test">
+          <img src="#" alt={p.name}/>
+          <p>{p.name}</p>
+          <button onClick={e => {props.removePokemon(index)}}>remove</button>
+          </div>)}
+      </div>  
+    )
+}
+
