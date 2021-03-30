2be757709e5094f6e612ae36412e4275e65ae6f4
unknown <marcus.larsen.swe@gmail.com>
2021-03-30 23:21:10 +0200

    built array with bild url

--- printaPokemon.js
+++ printaPokemon.js
@@ -5,7 +5,7 @@ export default function PrintaPokemon(props){
      return(
         <div>
         {props.poke.map((p, index) => <div key={p.name}  index={index} className="test">
-          <img src="#" alt={p.name}/>
+          <img src={p.bildUrl} alt={p.name}/>
           <p>{p.name}</p>
           <button onClick={e => {props.removePokemon(index)}}>remove</button>
           </div>)}
