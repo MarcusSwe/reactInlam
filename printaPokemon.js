daf7e05450f1511d86729ec1106d9daf2d061c5c
unknown <marcus.larsen.swe@gmail.com>
2021-03-31 22:07:11 +0200

    multipage test without router

--- printaPokemon.js
+++ printaPokemon.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function PrintaPokemon(props){
 
-     return(
-        <div>
-        {props.poke.map((p, index) => <div key={p.name}  index={index} className="test">
+const [sida, setSida] = useState(false);
+
+
+function PrintaPoks(props){  
+  return (
+    <div>
+          {props.poke.map((p, index) => <div key={p.name}  index={index} className="test">
           <img src={p.bildUrl} alt={p.name}/>
           <p>{p.name}</p>
           <button onClick={e => {props.removePokemon(index)}}>remove</button>
           </div>)}
+    </div>
+  )
+}
+
+function PrintaInfo(props){
+
+}
+
+function ChooseToPrint(){
+  if(!sida) return PrintaPoks(props);
+  else return <div>OMEGA</div>
+}
+
+
+     return(
+        <div>
+         {ChooseToPrint()}
       </div>  
     )
 }
