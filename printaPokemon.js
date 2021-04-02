93293ebe240556dde963528a1890ca434340497d
unknown <marcus.larsen.swe@gmail.com>
2021-04-03 00:31:34 +0200

    react final multipage without router plus fetch json

--- printaPokemon.js
+++ printaPokemon.js
@@ -6,20 +6,26 @@ let i = 0;
 
 export default function PrintaPokemon(props){
 
-const [sida, setSida] = useState(false);
+const [sida, setSida] = useState(false)
 
-const [Xpokemons, XsetPokemons] = useState()
+const [Xpokemons, XsetPokemons] = useState([])
 
 useEffect(() => {
   async function XfetchPokemons(){
     const resp = await fetch(y)
     const pokemon = await resp.json()    
-    const pokemon500 = pokemon.sprites.back_default;
+    const pokemon500 = [];
+    pokemon500.push(pokemon.abilities)
+    pokemon500.push(pokemon.sprites)
+    pokemon500.push(pokemon.height)
+    pokemon500.push(pokemon.moves)
+    //const pokemon500 = pokemon.sprites.back_default;
     XsetPokemons(pokemon500);      
     console.log(Xpokemons)
+    console.log("TEEEEEEEEEEEEEEEEEEEEST")
   }
   XfetchPokemons();        
-},);
+},[sida]);
 
 
 
@@ -27,10 +33,10 @@ function PrintaPoks(props){
   return (
     <div>
           {props.poke.map((p, index) => <div key={p.name}  index={index} className="test">
-          <img src={p.bildUrl} alt={p.name}/>
-          <p>{p.name}</p>
-          <button onClick={e => {props.removePokemon(index)}}>remove</button>
-          <button onClick={e => {setSida(true); x = p.name; y = p.url; i = index}}> test </button>
+          <img className="test2" src={p.bildUrl} alt={p.name}/>
+          <p>{p.name} </p>
+          <button onClick={e => {setSida(true); x = p.name; y = p.url; i = index}}> info </button>
+          <button onClick={e => {props.removePokemon(index)}}>remove</button>          
           </div>)}
     </div>
   )
@@ -39,28 +45,25 @@ function PrintaPoks(props){
 
 function PrintaInfo(props){ 
 
-  console.log(i);
-  console.log(Xpokemons);
+//{Xpokemons}  
+//
 
 return <div>   
-   <img src={Xpokemons} alt={x}/>
-   <p>{x}</p>
-   <p>{y}</p>
-   {Xpokemons}
-   <button onClick={e => {setSida(false); props.addUrl(i, Xpokemons)}}> back </button>
+   <img src={Xpokemons[1].back_default} alt={x}/>
+   <p><span className="titel">{x}</span></p>
+   <p>Abilites: {Xpokemons[0].map((p) => <span className="ability">{p.ability.name}<>, </></span>)} </p>
+   <p>Height: <span className="ability">{Xpokemons[2]}</span> </p>
+   <p>Moves: {Xpokemons[3].map((p) => <span className="ability">{p.move.name}<>, </></span>)} </p>
+
+   <button onClick={e => {setSida(false); props.addUrl(i, Xpokemons[1].back_default)}}> back </button>
    
    </div>   
 }
-
-//<button onClick={e => {setSida(false); props.addUrl(i, Xpokemons)}}> test </button>   
-
 
 function ChooseToPrint(){
   if(!sida) return PrintaPoks(props);
   else return PrintaInfo(props);
 }
-
-
      return(
         <div>
          {ChooseToPrint()}
